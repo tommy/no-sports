9b6deba2f66b7fac054077b84f7cf7567c1dca62
Tommy Rogers <tommysayshello@gmail.com>
2014-12-21 13:05:11 -0800

    Add exported listen! stream

--- src/clojure/no_sports/export.clj
+++ src/clojure/no_sports/export.clj
@@ -4,29 +4,30 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
             [clojure.string :refer [join]]
+            [clojure.pprint :refer [pprint]]
             [no-sports.twitter :refer :all]
             [no-sports.util :refer [remove-newlines]]))
 
-(defn- grade-fn
-  [manual]
-  (if manual
-    (fn [tweet]
-      (println "\n" (:text tweet) "[y/n]")
-      (read-line))
-    (constantly "")))
-
-(defn export
-  [& {:keys [manual out-file n max-id]
-      :or {manual false
-           out-file "lubbockonline.csv"
+(defn export-csv
+  "Export tweets to a csv for manual grading and use in model training."
+  [& {:keys [out-file page-size max-id]
+      :or {out-file "resources/lubbockonline.csv"
            max-id 526093035797364736
-           n 101}}]
-  (let [tweets (timeline 200 {:max-id max-id :count n})
+           page-size 101}}]
+  (let [tweets (timeline 200 {:max-id max-id :count page-size})
         line-fn (juxt :id
-                      (grade-fn manual)
+                      (constantly "")
                       (comp remove-newlines :text)
                       (comp :expanded_url first :urls :entities))]
     (with-open [out-file (io/writer out-file)]
       (csv/write-csv out-file
                      (map line-fn tweets)
                      :quote? (constantly true)))))
+
+(defn export-edn
+  "Export tweet objects from a user timeline to a file."
+  [n & {:keys [out-file page-size max-id]
+        :or {out-file "resources/lubbockonline.edn"
+             page-size (min n 101)}}]
+  (let [tweets (timeline n {:count page-size})]
+    (spit out-file (with-out-str (pprint tweets)))))
