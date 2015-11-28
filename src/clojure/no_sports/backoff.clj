00413938706bd7ed7e5d684672c37d21e232ee80
Tommy Rogers <tommysayshello@gmail.com>
2015-11-28 10:47:56 -0800

    Add backoff generator

---
+++ src/clojure/no_sports/backoff.clj
@@ -0,0 +1,47 @@
+(ns no-sports.backoff)
+
+(defn backoff
+  "Construct data object to track backoff."
+  [min-wait max-wait decay-time
+   & {:keys [timestamp power]
+      :or {timestamp (System/currentTimeMillis)}}]
+  {:min-wait min-wait
+   :max-wait max-wait
+   :decay-time decay-time
+   :timestamp timestamp
+   :attempt 0})
+
+(defn calculate-wait
+  "Calculates the next amount of time to wait. The value is
+  min-wait * (2 ** n)
+  subject to min and max wait times."
+  [{:keys [attempt min-wait max-wait]}]
+  (-> (Math/pow 2 attempt)
+      (* min-wait)
+      (min max-wait)
+      (max min-wait)
+      long))
+
+(defn decay
+  "Resets the attempts to zero if enough time has passed."
+  [backoff timestamp]
+  {:pre [timestamp]}
+  (let [diff (- timestamp (:timestamp backoff))]
+    (if (> diff (:decay-time backoff))
+      (assoc backoff :attempt 0)
+      backoff)))
+
+(defn split
+  "Branches the exponential backoff timer. Returns a tuple of
+  [wait-time new-backoff]. Uses the current time to determine
+  decay unless a timestamp is specified."
+  ([backoff]
+   (split backoff (System/currentTimeMillis)))
+  ([backoff timestamp]
+   (let [decayed-backoff (decay backoff timestamp)
+         wait-time (calculate-wait decayed-backoff)
+         new-backoff (-> decayed-backoff
+                         (update-in [:attempt] inc)
+                         (assoc :timestamp timestamp))]
+     [wait-time new-backoff])))
+
---
+++ test/no_sports/backoff_test.clj
@@ -0,0 +1,24 @@
+(ns no-sports.backoff-test
+  (:require [clojure.test :refer :all]
+            [no-sports.backoff :refer :all]))
+
+(deftest backoff-test
+  (testing "generate several backoff values"
+    (let [init (backoff 10 9999 10000 :timestamp 0)
+          [wait-1 b-1] (split init 10)
+          [wait-2 b-2] (split b-1 1000)
+          [wait-3 b-3] (split b-2 10000)
+          [wait-4 b-4] (split b-3 10001)
+          [wait-5 b-5] (split b-4 20002)]
+
+      (is (= [10 20 40 80 10] [wait-1 wait-2 wait-3 wait-4 wait-5]))
+      (are [k vs] (= vs (map k [init b-1 b-2 b-3 b-4 b-5]))
+        :attempt [0 1 2 3 4 1]
+        :timestamp [0 10 1000 10000 10001 20002])))
+
+  (testing "wait doesn't exceed max-wait"
+    (let [init (backoff 20 300 10000 :timestamp 0)
+          f (comp #(split % 0) second)
+          vs (iterate f [0 init])
+          waits (map first vs)]
+      (is (= [0 20 40 80 160 300 300] (take 7 waits))))))
