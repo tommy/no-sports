9d2d84d6b67a4e1405c69ca7b62c2f3e266dbe53
Tommy Rogers <tommysayshello@gmail.com>
2018-05-30 21:17:33 -0700

    Upgrade clojure and core.async

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://twitter.com/NoSportsAJ"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.4.474"]
                  [org.clojure/tools.logging "0.4.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.25"]
                  [org.apache.logging.log4j/log4j-core "2.11.0"]
